df50c9e4fa387157f07779a01eeefe00c585e45e
Tsubasa Nomura <tsubasa@nomupro.com>
2021-01-06 22:18:26 +0900

    Add setup.sh

---
+++ setup.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+set -eu
+
+if [ $# != 2 ]; then
+    echo "Resource Group or Location not specified."
+    exit 1
+fi
+
+rg=$1
+location=$2
+
+git clone https://github.com/tsubasaxZZZ/aks-demo-environment.git
+cd aks-demo-environment
+
+terraform init
+
+terraform apply -target random_string.uniqstr -var resource_group_name=$rg -var location=$location
+terraform plan -var resource_group_name=$rg -var location=$location -out plan.tfplan
+terraform apply plan.tfplan
